2b6e05df06b18fe8141be7364f16302075225903
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2026-01-30 12:59:06 +0300

    Fix the sntutils export bug

--- R/reexport_sntutils.R
+++ R/reexport_sntutils.R
@@ -3,8 +3,12 @@
 #' These functions are imported from sntutils and re-exported here
 #' for convenience in SNT Methods workflows.
 #'
+#' @inheritParams sntutils::calculate_reporting_metrics
+#' @inheritParams sntutils::classify_facility_activity
+#' @inheritParams sntutils::detect_outliers
+#' @inheritParams sntutils::get_active_facilities
+#'
 #' @name sntutils_reexports
-#' @keywords internal
 NULL
 
 #' @rdname sntutils_reexports
@@ -16,11 +20,6 @@ sntutils::calculate_reporting_metrics
 #' @importFrom sntutils classify_facility_activity
 #' @export
 sntutils::classify_facility_activity
-
-#' @rdname sntutils_reexports
-#' @importFrom sntutils detect_outliers
-#' @export
-sntutils::detect_outliers
 
 #' @rdname sntutils_reexports
 #' @importFrom sntutils detect_outliers
